aa106cce16c46b8ee4c6713f73de585b9cb59314
Rahmat Hafiz J. <rahmathafiz14@outlook.com>
2026-09-20 04:02:09 +0700

    Update SEO, GEO, AEO.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://skygraph.web.id',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
